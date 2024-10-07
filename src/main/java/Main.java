7f7294c3d6d8e225d03f0e328f62c1c6c4cb7057
Poharskyi <avpog.box@gmail.com>
2024-10-07 22:18:28 +0200

    add numbers generator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        double numberGen = Math.round(Math.random()*9)+1;
+        int numberLucky = (int) numberGen;
+        System.out.println("Your Lucky Number Today is " + numberLucky);
     }
 }
